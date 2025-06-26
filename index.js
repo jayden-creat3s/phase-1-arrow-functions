ee080ec20447ca4bc43abc2b185700da4f41beb7
jayden-creat3 <jaydenkamau508@gmail.com>
2025-06-27 00:01:49 +0300

    sbmit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Function expression to divide two numbers
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Example usage of divide function
+const result = divide(2000, 100); // This will return 20
+
+// Arrow function to square a number
+const square = (x) => x * x;
+
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
